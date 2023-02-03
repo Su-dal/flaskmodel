e3d899d24f37bbddd48424e76cbc058d6b0cd02c
Corpse-is-Talking <jjjjunho1011@gmail.com>
2023-02-03 11:45:43 +0900

    initial commit

---
+++ model.py
@@ -0,0 +1,6 @@
+import torch
+from transformers import PreTrainedTokenizerFast
+from transformers import BartForConditionalGeneration
+
+tokenizer = PreTrainedTokenizerFast.from_pretrained('digit82/kobart-summarization')
+model = BartForConditionalGeneration.from_pretrained('digit82/kobart-summarization')
